c0b3f1e4fe7729fed07af23a84607b2f84fb9ba6
James Armenta <armentajames@gmail.com>
2017-09-07 15:32:23 -0700

    Fix process.env Firebase keys

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -1,7 +1,11 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../src/private/firebaseServiceAccountKey.json') 
-|| process.env.FIREBASE_ACCOUNT_KEY;
+const serviceAccount = require('../../src/private/firebaseServiceAccountKey.json') ||
+  {
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
