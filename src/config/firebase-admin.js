a2617ea34ebc79e91a2bde1da800350b41fc8a15
James Armenta <armentajames@gmail.com>
2017-09-07 15:44:43 -0700

    Try & catch for local private keys file

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -1,11 +1,16 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../src/private/firebaseServiceAccountKey.json') ||
-  {
+let serviceAccount;
+
+try {
+  serviceAccount = require('../../src/private/firebaseServiceAccountKey.json');
+} catch (err) {
+  serviceAccount = {
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   };
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
