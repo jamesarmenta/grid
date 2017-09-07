2ff9c48501e34daaf6d862055cf32b7a0efaed47
James Armenta <armentajames@gmail.com>
2017-09-07 15:52:37 -0700

    Add FIREBASE_CLIENT_EMAIL env key

--- src/config/firebase-admin.js
+++ src/config/firebase-admin.js
@@ -9,6 +9,7 @@ try {
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
   };
 };
 
